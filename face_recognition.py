7f0ec7131f50ab69d645cad79c07d3fb1d55bda1
Eldeeb <omer84484@gmail.com>
2025-06-14 18:00:43 +0300

    Learning Face Recognition on Actors Photos (Deleted for copy wrights)

---
+++ face_recognition.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2 as cv
+
+haar_cascade = cv.CascadeClassifier('haar_cascade.xml')
+
+people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+face_recognizer.read('face_trained.yml')
+
+def predict_face(img):
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces = haar_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        face = gray[y:y+h, x:x+w]
+        label, confidence = face_recognizer.predict(face)
+        print(f'Label: {people[label]}, Confidence: {confidence}')
+        cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv.putText(img, people[label], (x, y-10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
+
+    return img
+
+img = cv.imread(r'E:\Abdo ElDeeb\Courses\Artificial Intelligence\Computer Vision\OpenCV-Learning\Faces\val\elton_john\1.jpg')
+predicted_img = predict_face(img)
+cv.imshow('Predicted Face', predicted_img)
+
+cv.waitKey(0)
---
+++ faces_train.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+import cv2 as cv
+
+people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+DIR = 'faces/train'
+
+features = [] # List to hold feature vectors
+labels = [] # List to hold labels
+
+def create_train():
+    for person in people:
+        img_dir = os.path.join(DIR, person)
+        label = people.index(person)
+        for img in os.listdir(img_dir):
+            img_path = os.path.join(img_dir, img)
+            image = cv.imread(img_path)
+            if image is not None:
+                gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+                face_cascade = cv.CascadeClassifier('haar_cascade.xml')
+                faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+                for (x, y, w, h) in faces:
+                    face = gray[y:y+h, x:x+w]
+                    features.append(face)
+                    labels.append(label)
+
+create_train()
+
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+face_recognizer.train(np.array(features, dtype='object'), np.array(labels))
+
+face_recognizer.save('face_trained.yml')
+
+# Convert features to numpy array with object dtype to handle different sized faces
+features_array = np.array(features, dtype=object)
+labels_array = np.array(labels)
+
+np.save('features.npy', features_array)
+np.save('labels.npy', labels_array)
+
